50c41b73f4553f0e954679588d67490c20f47664
Skay <irvur90@gmail.com>
2023-12-02 15:21:35 -0600

    corregir

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fn = () => {
     const miString = "soy un string!"
     let b = miString + " muy feliz"
+    const HolaMundo = "Hola Perros"
     console.log(b)
     console.log(HolaMundo)
 }
